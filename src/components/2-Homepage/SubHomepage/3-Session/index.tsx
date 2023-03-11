f9b916783fa547d61f2715d64b1de26e92975e22
Matheus <mathfernando@hotmail.com>
2023-03-11 12:49:05 -0300

    Session 3 zap

--- src/components/2-Homepage/SubHomepage/3-Session/index.tsx
+++ src/components/2-Homepage/SubHomepage/3-Session/index.tsx
@@ -106,7 +106,7 @@ export function LifeQuality() {
                                                         </FormControl>
 
                                                         <Link
-                                                            href={`https://wa.me/5511930003573?text=Olá, tudo bem? Meu nome é ${name} e meu telefone é ${number}. Acessei o site do Clube do Passaporte e gostaria de falar com um atendente.`} target="_blank"
+                                                            href={`https://wa.me/5511930003574?text=Olá, tudo bem? Meu nome é ${name} e meu telefone é ${number}. Acessei o site do Clube do Passaporte e gostaria de falar com um atendente.`} target="_blank"
                                                             _hover={{ textDecoration: 'none' }}>
                                                             <Flex
                                                                 bg='#25D366'
